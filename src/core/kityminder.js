75836c57e667fda3b6dd37a1cba96735b95e7d69
Akikonata <66875692@qq.com>
2013-12-18 15:27:50 +0800

    added impliment of  _initModules

--- src/core/kityminder.js
+++ src/core/kityminder.js
@@ -11,13 +11,50 @@ KityMinder.debug = true;
 
 // 模块注册
 KityMinder.registerModule = function( name, module ) {
-
+    //初始化模块列表
+    this._modules = this._modules||{};
+    this._modules[name] = module;
 };
 
 // 模块维护
 kity.extendClass(KityMinder, {
     _initModules: function() {
+        var me = this;
+        //在对象上挂接command池子
+        me.commands = {};
+        var _modules = KityMinder._modules;
+        if(_modules){
+            for(var key in _modules){
+                //执行模块初始化，抛出后续处理对象
+                var moduleDeals = 
+                    _modules[key].call(me);
+                console.log(moduleDeals);
 
+                moduleDeals["ready"]&&moduleDeals["ready"].call(this);
+
+                //command加入命令池子
+                var moduleDealsCommands = moduleDeals["commands"];
+                if(moduleDealsCommands){
+                    for(var _key in moduleDealsCommands){
+                        me.commands[_key] = moduleDealsCommands[_key];
+                    }
+                }
+                
+                //绑定事件
+                var moduleDealsEvents = moduleDeals["events"];
+                if(moduleDealsEvents){
+                    for(var _key in moduleDealsEvents){
+                        var bindEvs = _key.split(" ");
+                        console.log(bindEvs);
+                        var func = moduleDealsEvents[_key];
+                        for (var _i = 0; _i < bindEvs.length; _i++){
+                            me.on(bindEvs[_i],func);
+                        }
+                    }
+                }
+
+            }
+        }
     }
 });
 
