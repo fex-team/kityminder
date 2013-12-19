3e646f891928ae54278b0b5a71a7dd95fbdc2724
Akikonata <66875692@qq.com>
2013-12-19 19:02:09 +0800

    dev

--- minder_module/module.default.test.js
+++ minder_module/module.default.test.js
@@ -1,26 +1,33 @@
-var mindermoduleDefaultTest = function(){
-	console.log("test loaded");
-	var stroredData="stored";
+var mindermoduleDefaultTest = function() {
+    console.log("test loaded");
+    var stroredData = "stored";
 
-	return {
+    return {
         "commands": {
-        //todo:command字典，name－action  键值对模式编写
-            "testCommand":kity.createClass("testCommand",{
-                base:Command,
-                "execute":function(km,arg1,arg2,arg3){
-                    console.log(arg1,arg2,arg3);
+            //todo:command字典，name－action  键值对模式编写
+            "testCommand": kity.createClass("testCommand", {
+                base: Command,
+                "execute": function(km, arg1, arg2, arg3) {
+                    console.log(arg1, arg2, arg3);
                 }
             })
         },
 
         "events": {
             //todo:事件响应函数绑定列表,事件名-响应函数  键值对模式编写
-	        "click": function(e){
+            "click": function(e) {
                 window.alert("hahaha");
-	        },
-	        "keydown keyup": function(e){
+            },
+            "keydown keyup": function(e) {
                 window.alert("key");
-	        }
+            },
+            "beforecommand": function(e) {
+                console.log("precommand:", e);
+                e.cancel();
+            },
+            "command": function(e) {
+                console.log("command exec!", e);
+            }
         }
-	};
+    };
 };
