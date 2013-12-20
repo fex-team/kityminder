8b7c0bfa75bff33f221ebe81a0e33151b9669fbe
techird <techird@qq.com>
2013-12-20 18:19:01 +0800

    History Module

--- src/core/kityminder.js
+++ src/core/kityminder.js
@@ -78,25 +78,22 @@ kity.extendClass( KityMinder, ( function () {
             var me = this;
             var _action = new _commands[ name ]();
 
-            var args = arguments;
-            args[ 0 ] = this;
-            var _sendingArgs = ( function () {
-                var _args = [];
-                for ( var i = 1; i < args.length; i++ ) {
-                    _args.push( args[ i ] );
-                }
-                return _args;
-            } )();
+            var cmdArgs = Array.prototype.slice.call( arguments, 1 );
 
             var eventParams = {
                 command: _action,
                 commandName: name,
-                commandArgs: _sendingArgs
+                commandArgs: cmdArgs
             };
+
             var canceled = me._fire( new MinderEvent( 'beforecommand', eventParams, true ) );
+
             if ( !canceled ) {
+
                 me._fire( new MinderEvent( "precommand", eventParams, false ) );
-                _action.execute.apply( _action, args );
+
+                _action.execute.apply( _action, [ this ].concat( cmdArgs ) );
+
                 me._fire( new MinderEvent( "command", eventParams, false ) );
 
                 if ( _action.isContentChanged() ) {
