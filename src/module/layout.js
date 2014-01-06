6ea9d9ccd6f2bc67e01e8386d43e92af5b836a10
Akikonata <66875692@qq.com>
2014-01-06 15:48:16 +0800

    dev

--- src/module/layout.js
+++ src/module/layout.js
@@ -1,42 +1,47 @@
 KityMinder.registerModule( "LayoutModule", function () {
+	var createChildNode = function ( km, parent ) {
+		var _node = new MinderNode();
+		_node.setData( "y", Math.random() * 300 + 100 );
+		_node.setData( "text", "New Node" );
+		switch ( parent.branchside ) {
+		case "left":
+			break;
+		case "right":
+			break;
+		default:
+			( function () {
+				var children = parent.getChildren();
+				if ( children.length < 5 ) {
+					_node.setData( "x", parent.getData( "x" ) + 200 );
+					_node.setData( "align", "left" );
+				} else {
+					_node.setData( "x", parent.getData( "x" ) - 200 );
+					_node.setData( "align", "right" );
+				}
+			} )();
+			break;
+		}
+		parent.insertChild( _node );
+		km.execCommand( 'rendernode', _node );
+		return _node;
+	};
+
 	var CreateChildNodeCommand = kity.createClass( "CreateChildNodeCommand", ( function () {
 		return {
 			base: Command,
-			execute: function ( km, parent ) {
-				var _node = new MinderNode();
-				_node.setData( "y", Math.random() * 300 + 100 );
-				_node.setData( "text", "New Node" );
-				switch ( parent.branchside ) {
-				case "left":
-					break;
-				case "right":
-					break;
-				default:
-					( function () {
-						var children = parent.getChildren();
-						if ( children.length < 5 ) {
-							_node.setData( "x", parent.getData( "x" ) + 200 );
-							_node.setData( "align", "left" );
-						} else {
-							_node.setData( "x", parent.getData( "x" ) - 200 );
-							_node.setData( "align", "right" );
-						}
-					} )();
-					break;
-				}
-				parent.insertChild( _node );
-				km.execCommand( 'rendernode', _node );
-				return _node;
-			}
+			execute: createChildNode
 		};
 	} )() );
 
 	var CreateSiblingNodeCommand = kity.createClass( "CreateSiblingNodeCommand", ( function () {
 		return {
 			base: Command,
-			execute: function ( km, sibling, node ) {
+			execute: function ( km, sibling ) {
+				console.log( sibling );
 				var parent = sibling.getParent();
-				parent.insertChild( node );
+				if ( parent ) {
+					createChildNode( km, parent );
+				}
 			}
 		};
 	} )() );
