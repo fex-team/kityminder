b96701df39a09c030acf0e1d2a95c08db4e78148
Akikonata <66875692@qq.com>
2014-01-24 17:50:51 +0800

    added select

--- src/module/layout.js
+++ src/module/layout.js
@@ -110,8 +110,17 @@ KityMinder.registerModule( "LayoutModule", function () {
 			base: Command,
 			execute: function ( km ) {
 				var selectedNodes = km.getSelectedNodes();
-				console.log( selectedNodes );
+				var _buffer = [];
+				for ( var i = 0; i < selectedNodes.length; i++ ) {
+					_buffer.push( selectedNodes[ i ] );
+				}
+				while ( _buffer.length !== 1 ) {
+					var parent = _buffer[ 0 ].getParent();
+					if ( parent && _buffer.indexOf( parent ) === -1 ) _buffer.push( parent );
+					_buffer.shift();
+				}
 				km.removeNode( selectedNodes );
+				km.select( _buffer[ 0 ] );
 			}
 		};
 	} )() );
