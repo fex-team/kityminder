aeef6b0ce894bd11af2308170b51fdf5fa396630
campaign <zhanyi1022@gmail.com>
2014-02-05 15:29:58 +0800

    by zhanyi

---
+++ src/ui/combobox.js
@@ -0,0 +1,289 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: hn
+ * Date: 13-5-29
+ * Time: 下午8:01
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function(){
+
+    var widgetName = 'combobox',
+        itemClassName = 'kmui-combobox-item',
+        HOVER_CLASS = 'kmui-combobox-item-hover',
+        ICON_CLASS = 'kmui-combobox-checked-icon',
+        labelClassName = 'kmui-combobox-item-label';
+
+    KM.ui.define( widgetName, ( function(){
+
+        return {
+            tpl: "<ul class=\"dropdown-menu kmui-combobox-menu<%if (comboboxName!=='') {%> kmui-combobox-<%=comboboxName%><%}%>\" unselectable=\"on\" onmousedown=\"return false\" role=\"menu\" aria-labelledby=\"dropdownMenu\">" +
+                "<%if(autoRecord) {%>" +
+                "<%for( var i=0, len = recordStack.length; i<len; i++ ) {%>" +
+                "<%var index = recordStack[i];%>" +
+                "<li class=\"<%=itemClassName%><%if( selected == index ) {%> kmui-combobox-checked<%}%>\" data-item-index=\"<%=index%>\" unselectable=\"on\" onmousedown=\"return false\">" +
+                "<span class=\"kmui-combobox-icon\" unselectable=\"on\" onmousedown=\"return false\"></span>" +
+                "<label class=\"<%=labelClassName%>\" style=\"<%=itemStyles[ index ]%>\" unselectable=\"on\" onmousedown=\"return false\"><%=items[index]%></label>" +
+                "</li>" +
+                "<%}%>" +
+                "<%if( i ) {%>" +
+                "<li class=\"kmui-combobox-item-separator\"></li>" +
+                "<%}%>" +
+                "<%}%>" +
+                "<%for( var i=0, label; label = items[i]; i++ ) {%>" +
+                "<li class=\"<%=itemClassName%><%if( selected == i ) {%> kmui-combobox-checked<%}%> kmui-combobox-item-<%=i%>\" data-item-index=\"<%=i%>\" unselectable=\"on\" onmousedown=\"return false\">" +
+                "<span class=\"kmui-combobox-icon\" unselectable=\"on\" onmousedown=\"return false\"></span>" +
+                "<label class=\"<%=labelClassName%>\" style=\"<%=itemStyles[ i ]%>\" unselectable=\"on\" onmousedown=\"return false\"><%=label%></label>" +
+                "</li>" +
+                "<%}%>" +
+                "</ul>",
+            defaultOpt: {
+                //记录栈初始列表
+                recordStack: [],
+                //可用项列表
+                items: [],
+		        //item对应的值列表
+                value: [],
+                comboboxName: '',
+                selected: '',
+                //自动记录
+                autoRecord: true,
+                //最多记录条数
+                recordCount: 5
+            },
+            init: function( options ){
+
+                var me = this;
+
+                $.extend( me._optionAdaptation( options ), me._createItemMapping( options.recordStack, options.items ), {
+                    itemClassName: itemClassName,
+                    iconClass: ICON_CLASS,
+                    labelClassName: labelClassName
+                } );
+
+                this._transStack( options );
+
+                me.root( $( $.parseTmpl( me.tpl, options ) ) );
+
+                this.data( 'options', options ).initEvent();
+
+            },
+            initEvent: function(){
+
+                var me = this;
+
+                me.initSelectItem();
+
+                this.initItemActive();
+
+            },
+            /**
+             * 初始化选择项
+             */
+            initSelectItem: function(){
+
+                var me = this,
+                    labelClass = "."+labelClassName;
+
+                me.root().delegate('.' + itemClassName, 'click', function(){
+
+                    var $li = $(this),
+                        index = $li.attr('data-item-index');
+
+                    me.trigger('comboboxselect', {
+                        index: index,
+                        label: $li.find(labelClass).text(),
+                        value: me.data('options').value[ index ]
+                    }).select( index );
+
+                    me.hide();
+
+                    return false;
+
+                });
+
+            },
+            initItemActive: function(){
+                var fn = {
+                    mouseenter: 'addClass',
+                    mouseleave: 'removeClass'
+                };
+                if ($.IE6) {
+                    this.root().delegate( '.'+itemClassName,  'mouseenter mouseleave', function( evt ){
+                        $(this)[ fn[ evt.type ] ]( HOVER_CLASS );
+                    }).one('afterhide', function(){
+                    });
+                }
+            },
+            /**
+             * 选择给定索引的项
+             * @param index 项索引
+             * @returns {*} 如果存在对应索引的项，则返回该项；否则返回null
+             */
+            select: function( index ){
+
+                var itemCount = this.data('options').itemCount,
+                    items = this.data('options').autowidthitem;
+
+                if ( items && !items.length ) {
+                    items = this.data('options').items;
+                }
+
+                if( itemCount == 0 ) {
+                    return null;
+                }
+
+                if( index < 0 ) {
+
+                    index = itemCount + index % itemCount;
+
+                } else if ( index >= itemCount ) {
+
+                    index = itemCount-1;
+
+                }
+
+                this.trigger( 'changebefore', items[ index ] );
+
+                this._update( index );
+
+                this.trigger( 'changeafter', items[ index ] );
+
+                return null;
+
+            },
+            selectItemByLabel: function( label ){
+
+                var itemMapping = this.data('options').itemMapping,
+                    me = this,
+                    index = null;
+
+                !$.isArray( label ) && ( label = [ label ] );
+
+                $.each( label, function( i, item ){
+
+                    index = itemMapping[ item ];
+
+                    if( index !== undefined ) {
+
+                        me.select( index );
+                        return false;
+
+                    }
+
+                } );
+
+            },
+            /**
+             * 转换记录栈
+             */
+            _transStack: function( options ) {
+
+                var temp = [],
+                    itemIndex = -1,
+                    selected = -1;
+
+                $.each( options.recordStack, function( index, item ){
+
+                    itemIndex = options.itemMapping[ item ];
+
+                    if( $.isNumeric( itemIndex ) ) {
+
+                        temp.push( itemIndex );
+
+                        //selected的合法性检测
+                        if( item == options.selected ) {
+                            selected = itemIndex;
+                        }
+
+                    }
+
+                } );
+
+                options.recordStack = temp;
+                options.selected = selected;
+                temp = null;
+
+            },
+            _optionAdaptation: function( options ) {
+
+                if( !( 'itemStyles' in options ) ) {
+
+                    options.itemStyles = [];
+
+                    for( var i = 0, len = options.items.length; i < len; i++ ) {
+                        options.itemStyles.push('');
+                    }
+
+                }
+
+                options.autowidthitem = options.autowidthitem || options.items;
+                options.itemCount = options.items.length;
+
+                return options;
+
+            },
+            _createItemMapping: function( stackItem, items ){
+
+                var temp = {},
+                    result = {
+                        recordStack: [],
+                        mapping: {}
+                    };
+
+                $.each( items, function( index, item ){
+                    temp[ item ] = index;
+                } );
+
+                result.itemMapping = temp;
+
+                $.each( stackItem, function( index, item ){
+
+                    if( temp[ item ] !== undefined ) {
+                        result.recordStack.push( temp[ item ] );
+                        result.mapping[ item ] = temp[ item ];
+                    }
+
+                } );
+
+                return result;
+
+            },
+            _update: function ( index ) {
+
+                var options = this.data("options"),
+                    newStack = [],
+                    newChilds = null;
+
+                $.each( options.recordStack, function( i, item ){
+
+                    if( item != index ) {
+                        newStack.push( item );
+                    }
+
+                } );
+
+                //压入最新的记录
+                newStack.unshift( index );
+
+                if( newStack.length > options.recordCount ) {
+                    newStack.length = options.recordCount;
+                }
+
+                options.recordStack = newStack;
+                options.selected = index;
+
+                newChilds = $( $.parseTmpl( this.tpl, options ) );
+
+                //重新渲染
+                this.root().html( newChilds.html() );
+
+                newChilds = null;
+                newStack = null;
+
+            }
+        };
+
+    } )(), 'menu' );
+
+})();
---
+++ src/ui/dropmenu.js
@@ -0,0 +1,84 @@
+//dropmenu 类
+KM.ui.define('dropmenu', {
+    tmpl: '<ul class="kmui-dropdown-menu" aria-labelledby="dropdownMenu" >' +
+        '<%for(var i=0,ci;ci=data[i++];){%>' +
+        '<%if(ci.divider){%><li class="kmui-divider"></li><%}else{%>' +
+        '<li <%if(ci.active||ci.disabled){%>class="<%= ci.active|| \'\' %> <%=ci.disabled||\'\' %>" <%}%> data-value="<%= ci.value%>">' +
+        '<a href="#" tabindex="-1"><em class="kmui-dropmenu-checkbox"><i class="kmui-icon-ok"></i></em><%= ci.label%></a>' +
+        '</li><%}%>' +
+        '<%}%>' +
+        '</ul>',
+    defaultOpt: {
+        data: [],
+        click: function () {
+
+        }
+    },
+    init: function (options) {
+        var me = this;
+        var eventName = {
+            click: 1,
+            mouseover: 1,
+            mouseout: 1
+        };
+
+        this.root($($.parseTmpl(this.tmpl, options))).on('click', 'li[class!="kmui-disabled kmui-divider kmui-dropdown-submenu"]',function (evt) {
+            $.proxy(options.click, me, evt, $(this).data('value'), $(this))()
+        }).find('li').each(function (i, el) {
+                var $this = $(this);
+                if (!$this.hasClass("kmui-disabled kmui-divider kmui-dropdown-submenu")) {
+                    var data = options.data[i];
+                    $.each(eventName, function (k) {
+                        data[k] && $this[k](function (evt) {
+                            $.proxy(data[k], el)(evt, data, me.root)
+                        })
+                    })
+                }
+            })
+
+    },
+    disabled: function (cb) {
+        $('li[class!=kmui-divider]', this.root()).each(function () {
+            var $el = $(this);
+            if (cb === true) {
+                $el.addClass('kmui-disabled')
+            } else if ($.isFunction(cb)) {
+                $el.toggleClass('kmui-disabled', cb(li))
+            } else {
+                $el.removeClass('kmui-disabled')
+            }
+
+        });
+    },
+    val: function (val) {
+        var currentVal;
+        $('li[class!="kmui-divider kmui-disabled kmui-dropdown-submenu"]', this.root()).each(function () {
+            var $el = $(this);
+            if (val === undefined) {
+                if ($el.find('em.kmui-dropmenu-checked').length) {
+                    currentVal = $el.data('value');
+                    return false
+                }
+            } else {
+                $el.find('em').toggleClass('kmui-dropmenu-checked', $el.data('value') == val)
+            }
+        });
+        if (val === undefined) {
+            return currentVal
+        }
+    },
+    addSubmenu: function (label, menu, index) {
+        index = index || 0;
+
+        var $list = $('li[class!=kmui-divider]', this.root());
+        var $node = $('<li class="kmui-dropdown-submenu"><a tabindex="-1" href="#">' + label + '</a></li>').append(menu);
+
+        if (index >= 0 && index < $list.length) {
+            $node.insertBefore($list[index]);
+        } else if (index < 0) {
+            $node.insertBefore($list[0]);
+        } else if (index >= $list.length) {
+            $node.appendTo($list);
+        }
+    }
+}, 'menu');
