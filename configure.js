fd752eb7c4bd94c251816b1108ac1b2ee2b5eecf
campaign <zhanyi1022@gmail.com>
2014-02-10 23:03:39 +0800

    by zhanyi

--- configure.js
+++ configure.js
@@ -1,8 +1,18 @@
 window.KITYMINDER_CONFIG = {
+    //定义工具栏
+    toolbars:[
+        'undo redo | bold italic | forecolor'
+    ]
+
     //设置主题
     //layoutstyle : 'default'
 
     //回退相关选项
     //maxUndoCount:20  //最大可回退的次数，默认20
 
+    //语言默认是zh-cn
+    //,lang:'zh-cn'
+
+    //设置km整体的z-index大小
+    //zIndex : 1000
 };
