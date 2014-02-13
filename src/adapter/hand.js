a4b3a6f88761b7582c9ba3a17454912e1478833b
techird <techird@qq.com>
2014-02-13 18:27:23 +0800

    hand

---
+++ src/adapter/hand.js
@@ -0,0 +1,27 @@
+KM.registerUI( 'hand',
+    function ( name ) {
+        var me = this;
+        var $btn = $.kmuibutton( {
+            icon: name,
+            click: function ( e ) {
+                var drag = me._onDragMode = !me._onDragMode;
+                me._paper.setStyle( 'cursor', drag ? 'pointer' : 'default' );
+                me._paper.setStyle( 'cursor', drag ? '-webkit-grab' : 'default' );
+                $btn.kmui().active( drag );
+                if ( drag ) {
+                    me._paper.drag();
+                } else {
+                    me._paper.undrag();
+                }
+            },
+            title: this.getLang( 'hand' )[ name ] || ''
+        } );
+        me.on( 'beforemousemove', function ( e ) {
+            if ( this._onDragMode ) {
+                e.stopPropagation();
+            }
+        } );
+        kity.extendClass( kity.Paper, kity.Draggable );
+        return $btn;
+    }
+);
