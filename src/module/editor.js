59e98a0c3c6c37502863733275f64414558496bd
campaign <zhanyi1022@gmail.com>
2014-02-17 00:16:51 +0800

    by zhanyi

--- src/module/editor.js
+++ src/module/editor.js
@@ -1,4 +1,5 @@
 KityMinder.registerModule( "TextEditModule", function () {
+    var km = this;
     var sel = new Minder.Selection();
     var receiver = new Minder.Receiver(this);
     var range = new Minder.Range();
@@ -10,6 +11,9 @@ KityMinder.registerModule( "TextEditModule", function () {
     var oneTime = 0;
 
     var lastEvtPosition,dir = 1;
+
+
+
     return {
         //插入光标
         "init":function(){
@@ -51,12 +55,11 @@ KityMinder.registerModule( "TextEditModule", function () {
                 }
                 mouseDownStatus = false;
                 oneTime = 0;
-
             },
-            'mousemove':function(e){
-
-                setTimeout(function(){
-                    if(mouseDownStatus){
+            'beforemousemove':function(e){
+                if(mouseDownStatus){
+                    e.stopPropagationImmediately();
+                    setTimeout(function(){
 
                         var offset = e.getPosition();
                         dir = offset.x > lastEvtPosition.x  ? 1 : (offset.x  < lastEvtPosition.x ? -1 : dir);
@@ -64,8 +67,9 @@ KityMinder.registerModule( "TextEditModule", function () {
                             .updateSelectionShow(dir);
                         sel.stroke('none',0);
                         lastEvtPosition = e.getPosition();
-                    }
-                },100)
+                    },100)
+                }
+
 
             },
             'restoreScene':function(){
