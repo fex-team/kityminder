0c1f2f042b7c6b3436a93ffc3833d5aa1c03fbd1
Akikonata <66875692@qq.com>
2014-02-19 00:38:50 +0800

    changed rule

--- src/module/layout.default.js
+++ src/module/layout.default.js
@@ -121,7 +121,7 @@ KityMinder.registerModule( "LayoutDefault", function () {
 		case "sub":
 			var underline = Layout.underline = new kity.Path();
 			var highlightshape = Layout.highlightshape = new kity.Rect().setRadius( 4 );
-			node.getBgRc().clear().addShapes( [ Layout.bgRect = new kity.Rect().setRadius(4), highlightshape, underline ] );
+			node.getBgRc().clear().addShapes( [ Layout.bgRect = new kity.Rect().setRadius( 4 ), highlightshape, underline ] );
 			break;
 		default:
 			break;
@@ -170,7 +170,7 @@ KityMinder.registerModule( "LayoutDefault", function () {
 			Layout.highlightshape
 				.setWidth( _contWidth + nodeStyle.padding[ 1 ] + nodeStyle.padding[ 3 ] )
 				.setHeight( _contHeight + nodeStyle.padding[ 0 ] + nodeStyle.padding[ 2 ] );
-			Layout.bgRect.setWidth(width).setHeight(height);
+			Layout.bgRect.setWidth( width ).setHeight( height );
 			break;
 		default:
 			break;
@@ -475,6 +475,16 @@ KityMinder.registerModule( "LayoutDefault", function () {
 				Layout.align = siblingLayout.align;
 				parent.insertChild( node, sibling.getIndex() + 1 );
 				if ( parent.getType() === "root" ) {
+					var len = parent.getChildren().length;
+					if ( len < 7 ) {
+						if ( len % 2 ) {
+							Layout.appendside = "right";
+							Layout.align = "left";
+						} else {
+							Layout.appendside = "left";
+							Layout.align = "right";
+						}
+					}
 					var sideList = parentLayout[ Layout.appendside + "List" ];
 					var idx = sideList.indexOf( sibling );
 					sideList.splice( idx + 1, 0, node );
