6224251282a955de9a94551ead21157a38bfd828
Akikonata <66875692@qq.com>
2014-04-08 02:09:47 +0800

    publish

--- dist/kityminder.all.js
+++ dist/kityminder.all.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * kityminder - v1.0.0 - 2014-03-28
+ * kityminder - v1.0.0 - 2014-04-08
  * https://github.com/fex-team/kityminder
  * GitHub: https://github.com/fex-team/kityminder.git 
  * Copyright (c) 2014 f-cube @ FEX; Licensed MIT
