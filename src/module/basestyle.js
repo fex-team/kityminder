d36bd9e3bf080343ffb7802c9bd0e17410db5d23
Akikonata <66875692@qq.com>
2014-04-14 14:39:07 +0800

    fixed readonly

--- src/module/basestyle.js
+++ src/module/basestyle.js
@@ -6,70 +6,70 @@ KityMinder.registerModule( "basestylemodule", function () {
             "bold": kity.createClass( "boldCommand", {
                 base: Command,
 
-                execute: function (  ) {
+                execute: function () {
 
                     var nodes = km.getSelectedNodes();
-                    if(this.queryState('bold') == 1){
-                        utils.each(nodes,function(i,n){
-                            n.setData('bold');
-                            n.getTextShape().setAttr('font-weight');
-                            km.updateLayout(n)
-                        })
-                    }else{
-                        utils.each(nodes,function(i,n){
-                            n.setData('bold',true);
-                            n.getTextShape().setAttr('font-weight','bold');
-                            km.updateLayout(n)
-                        })
+                    if ( this.queryState( 'bold' ) == 1 ) {
+                        utils.each( nodes, function ( i, n ) {
+                            n.setData( 'bold' );
+                            n.getTextShape().setAttr( 'font-weight' );
+                            km.updateLayout( n )
+                        } )
+                    } else {
+                        utils.each( nodes, function ( i, n ) {
+                            n.setData( 'bold', true );
+                            n.getTextShape().setAttr( 'font-weight', 'bold' );
+                            km.updateLayout( n )
+                        } )
                     }
                 },
-                queryState: function (  ) {
+                queryState: function () {
                     var nodes = km.getSelectedNodes(),
                         result = 0;
-                    if(nodes.length == 0){
+                    if ( nodes.length == 0 ) {
                         return -1;
                     }
-                    utils.each(nodes,function(i,n){
-                        if( n && n.getData('bold') ){
+                    utils.each( nodes, function ( i, n ) {
+                        if ( n && n.getData( 'bold' ) ) {
                             result = 1;
                             return false;
                         }
-                    });
+                    } );
                     return result;
                 }
             } ),
             "italic": kity.createClass( "italicCommand", {
                 base: Command,
 
-                execute: function (  ) {
+                execute: function () {
 
                     var nodes = km.getSelectedNodes();
-                    if(this.queryState('italic') == 1){
-                        utils.each(nodes,function(i,n){
-                            n.setData('italic');
-                            n.getTextShape().setAttr('font-style');
-                            km.updateLayout(n)
-                        })
-                    }else{
-                        utils.each(nodes,function(i,n){
-                            n.setData('italic',true);
-                            n.getTextShape().setAttr('font-style','italic');
-                            km.updateLayout(n)
-                        })
+                    if ( this.queryState( 'italic' ) == 1 ) {
+                        utils.each( nodes, function ( i, n ) {
+                            n.setData( 'italic' );
+                            n.getTextShape().setAttr( 'font-style' );
+                            km.updateLayout( n )
+                        } )
+                    } else {
+                        utils.each( nodes, function ( i, n ) {
+                            n.setData( 'italic', true );
+                            n.getTextShape().setAttr( 'font-style', 'italic' );
+                            km.updateLayout( n )
+                        } )
                     }
                 },
-                queryState: function (  ) {
+                queryState: function () {
                     var nodes = km.getSelectedNodes(),
                         result = 0;
-                    if(nodes.length == 0){
+                    if ( nodes.length == 0 ) {
                         return -1;
                     }
-                    utils.each(nodes,function(i,n){
-                        if( n && n.getData('italic') ){
+                    utils.each( nodes, function ( i, n ) {
+                        if ( n && n.getData( 'italic' ) ) {
                             result = 1;
                             return false;
                         }
-                    });
+                    } );
                     return result;
                 }
             } )
@@ -81,12 +81,12 @@ KityMinder.registerModule( "basestylemodule", function () {
         "events": {
             "beforeRenderNode": function ( e ) {
                 //加粗
-                if(e.node.getData('bold')){
-                    e.node.getTextShape().setAttr('font-weight','bold');
+                if ( e.node.getData( 'bold' ) ) {
+                    e.node.getTextShape().setAttr( 'font-weight', 'bold' );
                 }
 
-                if(e.node.getData('italic')){
-                    e.node.getTextShape().setAttr('font-style','italic');
+                if ( e.node.getData( 'italic' ) ) {
+                    e.node.getTextShape().setAttr( 'font-style', 'italic' );
                 }
             }
         }
