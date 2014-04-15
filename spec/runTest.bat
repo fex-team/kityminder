5bbe38deb445938c1f06d4683e7611fc7db34662
yancend <dongyancen@baidu.com>
2014-04-15 16:58:43 +0800

    [test]添加手动用例 test list for 1.1.2.xmind

--- spec/runTest.bat
+++ spec/runTest.bat
@@ -3,4 +3,3 @@ del /q /s .\coverage\json_files\*.json
 for /f %%j in ('karma start ./karma.conf.js') do set karmaRe=%%j
 for /f  %%i in ('node ./mergeCoverageForIstanbul.js') do set re=%%i
 istanbul report html coverage/json_files/%re%.json
-pause
