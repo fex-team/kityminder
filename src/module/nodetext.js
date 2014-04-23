d71c446546ffdbee60566b67716b2657ae1761c1
campaign <zhanyi1022@gmail.com>
2014-04-23 21:05:02 +0800

    去掉debugger

--- src/module/nodetext.js
+++ src/module/nodetext.js
@@ -1,18 +1,18 @@
 KityMinder.registerModule( "NodeText", function () {
     var km = this;
     return {
-        events: {
-            'renderNodeCenter': function ( e ) {
+        events:{
+            'renderNodeCenter':function(e){
 
                 var node = e.node;
-                if ( node.getType() == 'root' )
-                    var width = node.getContRc().getWidth();
+
+                var width = node.getContRc().getWidth();
                 var textShape = new kity.Text( node.getData( 'text' ) || '' );
                 textShape.setAttr( '_nodeTextShape', true );
                 node.getContRc().appendShape( textShape );
-                var style = this.getCurrentLayoutStyle()[ node.getType() ];
-                textShape.fill( style.color ).setSize( style.fontSize );
-                textShape.setPosition( width, textShape.getHeight() * .75 )
+                var style = this.getCurrentLayoutStyle()[node.getType()];
+                textShape.fill(style.color).setSize(style.fontSize);
+                textShape.setPosition(width,textShape.getHeight()*.75)
             }
         }
     }
