83f109be0bff2b6e90eb8111cb1e691fc830c4dd
Zswang <wjhu111@21cn.com>
2014-05-04 21:39:32 +0800

    添加编辑节点的快捷键和菜单

--- src/core/keymap.js
+++ src/core/keymap.js
@@ -3,41 +3,45 @@ var keymap = KityMinder.keymap  = {
     'Tab' : 9,
     'Enter' : 13,
 
-    'Shift':16,
-    'Control':17,
-    'Alt':18,
-    'CapsLock':20,
+    'Shift' : 16,
+    'Control' : 17,
+    'Alt' : 18,
+    'CapsLock' : 20,
 
-    'Esc':27,
+    'Esc': 27,
 
-    'Spacebar':32,
+    'Spacebar' : 32,
 
-    'PageUp':33,
-    'PageDown':34,
-    'End':35,
-    'Home':36,
+    'PageUp' : 33,
+    'PageDown' : 34,
+    'End' : 35,
+    'Home' : 36,
 
-    'Left':37,
-    'Up':38,
-    'Right':39,
-    'Down':40,
+    'Left' : 37,
+    'Up' : 38,
+    'Right' : 39,
+    'Down' : 40,
 
-    'Insert':45,
+    'Insert' : 45,
 
-    'Del':46,
+    'Del' : 46,
 
-    'NumLock':144,
+    'NumLock' : 144,
 
-    'Cmd':91,
+    'Cmd' : 91,
 
-    '=':187,
-    '-':189,
+    'F2' : 113,
+    'F3' : 114,
+    'F4' : 115,
 
-    "b":66,
-    'i':73,
+    '=' : 187,
+    '-' : 189,
+
+    "b" : 66,
+    'i' : 73,
     //回退
-    'z':90,
-    'y':89,
+    'z' : 90,
+    'y' : 89,
     //粘贴
     'v' : 86,
     'x' : 88,
--- src/module/layout.js
+++ src/module/layout.js
@@ -195,12 +195,33 @@ KityMinder.registerModule( "LayoutModule", function () {
 			}
 		};
 	} )() );
+	var EditNodeCommand = kity.createClass( "EditNodeCommand", ( function () {
+		return {
+			base: Command,
+			execute: function ( km ) {
+				var selectedNode = km.getSelectedNode();
+				if ( !selectedNode ) {
+					return null;
+				}
+				km.select( selectedNode, true );
+			},
+			queryState: function ( km ) {
+				var selectedNode = km.getSelectedNode();
+				if ( !selectedNode ) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+		};
+	} )() );
 
 	return {
 		"commands": {
 			"appendchildnode": AppendChildNodeCommand,
 			"appendsiblingnode": AppendSiblingNodeCommand,
 			"removenode": RemoveNodeCommand,
+			"editnode": EditNodeCommand,
 			"switchlayout": SwitchLayoutCommand
 		},
 		"events": {
@@ -237,6 +258,12 @@ KityMinder.registerModule( "LayoutModule", function () {
 				},
 				cmdName: 'appendchildnode'
 			}, {
+				label: this.getLang( 'node.editnode' ),
+				exec: function () {
+					this.execCommand( 'editnode', null );
+				},
+				cmdName: 'editnode'
+			}, {
 				label: this.getLang( 'node.removenode' ),
 				cmdName: 'removenode'
 			}, {
@@ -249,6 +276,7 @@ KityMinder.registerModule( "LayoutModule", function () {
 			"node": {
 				'appendsiblingnode': 'appendsiblingnode',
 				'appendchildnode': 'appendchildnode',
+				'editnode': 'editnode',
 				'removenode': 'removenode'
 			},
 			'defaultExpand': {
