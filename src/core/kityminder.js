12e75a2f1d5083fa879e8a61f3c7c060c8f52e0a
campaign <zhanyi1022@gmail.com>
2014-05-04 18:02:28 +0800

    更新版本为1.1.3

--- src/core/kityminder.js
+++ src/core/kityminder.js
@@ -1,7 +1,7 @@
 var KityMinder = window.KM = window.KityMinder = function () {
 	var instanceMap = {}, instanceId = 0;
 	return {
-		version: '1.2.0',
+		version: '1.1.3',
 		createMinder: function ( renderTarget, options ) {
 			options = options || {};
 			options.renderTo = Utils.isString( renderTarget ) ? document.getElementById( renderTarget ) : renderTarget;
