32b2c3cdfa404acb7981ed2beb5e5e9afdf864bf
Akikonata <66875692@qq.com>
2014-05-06 10:54:45 +0800

    修改文案

--- dialogs/preference/preference.js
+++ dialogs/preference/preference.js
@@ -2,45 +2,47 @@
     //todo 这里先写死成中文
     var preferenceContent = '<div class="help-content" style="padding:20px;width:360px;">';
     preferenceContent += '<h3>展开属性设置</h3>';
-    preferenceContent += '<p><label><input type="checkbox" name="expand" />是否全部展开</label></p>';
+    preferenceContent += '<p><label><input type="checkbox" name="expand" />打开时展开全部节点（刷新页面生效）</label></p>';
     preferenceContent += '<p><hr/></p>';
-    preferenceContent += '<p><button id="reset_preference">回复默认值</button></p>';
-    preferenceContent += '<p><label><b style="color:red">有些偏好设置会在你下次刷新页面时生效</b></label></p>';
+    preferenceContent += '<p><button id="reset_preference">重置</button></p>';
+    //preferenceContent += '<p><label><b style="color:red">有些偏好设置会在你下次刷新页面时生效</b></label></p>';
     preferenceContent += '</div>';
 
 
     //todo 偏好设置暂时都在这里处理
     //用于在reset所有偏好时，清除这个dialog下的所有偏好
-    var allPreferences ={};
+    var allPreferences = {};
 
-    function checkEverything(km,$w){
-        checkExpand(km,$w)
+    function checkEverything( km, $w ) {
+        checkExpand( km, $w )
     }
-    function initEverything(km,$w){
-        initExpand(km,$w)
+
+    function initEverything( km, $w ) {
+        initExpand( km, $w )
     }
 
     //展开
-    function initExpand(km,$w){
-        var expand = km.getOptions('defaultExpand');
-        $w.find('[name=expand]')[0].checked =expand && expand.defaultLayer == 0;
-        allPreferences['defaultExpand'] = null;
+    function initExpand( km, $w ) {
+        var expand = km.getOptions( 'defaultExpand' );
+        $w.find( '[name=expand]' )[ 0 ].checked = expand && expand.defaultLayer == 0;
+        allPreferences[ 'defaultExpand' ] = null;
     }
-    function checkExpand(km,$w){
-        var checked = $w.find('[name=expand]')[0].checked;
-        if(checked){
-            km.setPreferences('defaultExpand',{
+
+    function checkExpand( km, $w ) {
+        var checked = $w.find( '[name=expand]' )[ 0 ].checked;
+        if ( checked ) {
+            km.setPreferences( 'defaultExpand', {
                 'defaultLayer': 0,
                 'defaultSubShow': 0
-            })
-        }else{
-            km.setPreferences('defaultExpand')
+            } )
+        } else {
+            km.setPreferences( 'defaultExpand' )
         }
     }
 
     //重置偏好
-    function resetPreferences(km){
-        km.setPreferences(allPreferences)
+    function resetPreferences( km ) {
+        km.setPreferences( allPreferences )
     }
 
     KM.registerWidget( 'preference', {
@@ -56,17 +58,17 @@
         },
         initEvent: function ( km, $w ) {
             //绑定默认值
-            initEverything(km,$w);
+            initEverything( km, $w );
 
-            $w.find('#reset_preference').on( 'click', function ( e ) {
-                resetPreferences(km);
+            $w.find( '#reset_preference' ).on( 'click', function ( e ) {
+                resetPreferences( km );
                 //重置系统默认的偏好设置
-                initEverything(km,$w)
+                initEverything( km, $w )
             } );
 
             $w.on( 'click', '.kmui-close', function ( e ) {
-                checkEverything(km,$w);
-                km.fire('preferencechange')
+                checkEverything( km, $w );
+                km.fire( 'preferencechange' )
             } );
         },
         width: 400
