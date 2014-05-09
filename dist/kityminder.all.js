02cb6e78539db151277dbf9366ccc2f868a2c9b8
Akikonata <66875692@qq.com>
2014-05-09 18:16:32 +0800

    grunt

--- dist/kityminder.all.js
+++ dist/kityminder.all.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * kityminder - v1.0.0 - 2014-05-09
+ * kityminder - v1.1.3 - 2014-05-09
  * https://github.com/fex-team/kityminder
  * GitHub: https://github.com/fex-team/kityminder.git 
  * Copyright (c) 2014 f-cube @ FEX; Licensed MIT
