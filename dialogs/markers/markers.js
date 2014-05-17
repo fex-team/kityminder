6d8c0fcdcac456599ecc4fd02ca162a0dbe122dc
Akikonata <66875692@qq.com>
2014-05-17 19:15:23 +0800

    added percent

--- dialogs/markers/markers.js
+++ dialogs/markers/markers.js
@@ -13,7 +13,9 @@
             ".kmui-dialog-<%= container %> .icon.p9{background-position: -160px 0}" +
             ".kmui-dialog-<%= container %> .icon.p0{background-position: -180px 0}" +
             ".kmui-dialog-<%= container %> ul li{width:40%;display:inline-block}" +
+            ".kmui-dialog-<%= container %> h4{padding:5px 10px; margin:0; background:#eee}" +
             "</style>" +
+            "<h4><%= priority %></h3>" +
             "<ul class='icon-list priority'>" +
             "<li value='1' type='priority'><span class='icon p1'></span><span><%= priority %>1</span></li>" +
             "<li value='2' type='priority'><span class='icon p2'></span><span><%= priority %>2</span></li>" +
@@ -24,14 +26,20 @@
             "<li value='7' type='priority'><span class='icon p7'></span><span><%= priority %>7</span></li>" +
             "<li value='8' type='priority'><span class='icon p8'></span><span><%= priority %>8</span></li>" +
             "<li value='9' type='priority'><span class='icon p9'></span><span><%= priority %>9</span></li>" +
-            "<li value='10' type='priority'><span class='icon p0'></span><span><%= priority %>-</span></li>" +
+            "<li value='0' type='priority'><span class='icon p0'></span><span><%= none %></span></li>" +
             "</ul>" +
+            "<h4><%= progress.title %></h4>" +
             "<ul class='icon-list progress'>" +
             "<li value='1' type='progress'><span class='icon p1'></span><span><%= progress.notdone %></span></li>" +
-            "<li value='2' type='progress'><span class='icon p2'></span><span><%= progress.quarterdone %></span></li>" +
-            "<li value='3' type='progress'><span class='icon p3'></span><span><%= progress.halfdone %></span></li>" +
-            "<li value='4' type='progress'><span class='icon p4'></span><span><%= progress.threequartersdone %></span></li>" +
-            "<li value='5' type='progress'><span class='icon p5'></span><span><%= progress.done %></span></li>" +
+            "<li value='2' type='progress'><span class='icon p2'></span><span><%= progress.done1 %></span></li>" +
+            "<li value='3' type='progress'><span class='icon p3'></span><span><%= progress.done2 %></span></li>" +
+            "<li value='4' type='progress'><span class='icon p4'></span><span><%= progress.done3 %></span></li>" +
+            "<li value='5' type='progress'><span class='icon p5'></span><span><%= progress.done4 %></span></li>" +
+            "<li value='6' type='progress'><span class='icon p6'></span><span><%= progress.done5 %></span></li>" +
+            "<li value='7' type='progress'><span class='icon p7'></span><span><%= progress.done6 %></span></li>" +
+            "<li value='8' type='progress'><span class='icon p8'></span><span><%= progress.done7 %></span></li>" +
+            "<li value='9' type='progress'><span class='icon p9'></span><span><%= progress.done %></span></li>" +
+            "<li value='0' type='progress'><span class='icon p0'></span><span><%= none %></span></li>" +
             "</ul>",
         initContent: function (km, $w) {
             var lang = km.getLang('dialogs.markers');
