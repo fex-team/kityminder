b6a7a3db74df84b33186eabcbce784c6c7ef2453
Akikonata <66875692@qq.com>
2014-05-17 16:28:05 +0800

    changed icons

--- dialogs/markers/markers.js
+++ dialogs/markers/markers.js
@@ -7,6 +7,12 @@
             ".kmui-dialog-<%= container %> .icon.p3{background-position: -40px 0}" +
             ".kmui-dialog-<%= container %> .icon.p4{background-position: -60px 0}" +
             ".kmui-dialog-<%= container %> .icon.p5{background-position: -80px 0}" +
+            ".kmui-dialog-<%= container %> .icon.p6{background-position: -100px 0}" +
+            ".kmui-dialog-<%= container %> .icon.p7{background-position: -120px 0}" +
+            ".kmui-dialog-<%= container %> .icon.p8{background-position: -140px 0}" +
+            ".kmui-dialog-<%= container %> .icon.p9{background-position: -160px 0}" +
+            ".kmui-dialog-<%= container %> .icon.p0{background-position: -180px 0}" +
+            ".kmui-dialog-<%= container %> ul li{width:40%;display:inline-block}" +
             "</style>" +
             "<ul class='icon-list priority'>" +
             "<li value='1' type='priority'><span class='icon p1'></span><span><%= priority %>1</span></li>" +
@@ -14,6 +20,11 @@
             "<li value='3' type='priority'><span class='icon p3'></span><span><%= priority %>3</span></li>" +
             "<li value='4' type='priority'><span class='icon p4'></span><span><%= priority %>4</span></li>" +
             "<li value='5' type='priority'><span class='icon p5'></span><span><%= priority %>5</span></li>" +
+            "<li value='6' type='priority'><span class='icon p6'></span><span><%= priority %>6</span></li>" +
+            "<li value='7' type='priority'><span class='icon p7'></span><span><%= priority %>7</span></li>" +
+            "<li value='8' type='priority'><span class='icon p8'></span><span><%= priority %>8</span></li>" +
+            "<li value='9' type='priority'><span class='icon p9'></span><span><%= priority %>9</span></li>" +
+            "<li value='0' type='priority'><span class='icon p0'></span><span><%= priority %>0</span></li>" +
             "</ul>" +
             "<ul class='icon-list progress'>" +
             "<li value='1' type='progress'><span class='icon p1'></span><span><%= progress.notdone %></span></li>" +
